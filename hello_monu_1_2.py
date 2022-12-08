e1b95b5947913da6877076f5dbfeea764d1e107a
Yashveer325007 <119954921+Yashveer325007@users.noreply.github.com>
2022-12-08 11:22:04 +0530

    Add files via upload

---
+++ hello_monu_1_2.py
@@ -0,0 +1,31 @@
+# twinkle twinkle little star
+print('''Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky''')
+#program to add two numbers
+a=24
+b=32
+print(a+b)
+#program to find remainder of a divided by b
+a=24345
+b=114
+print("remainder os a and b is", a%b)
+#type of variable
+a=input("enter your age")
+print(type(a))
+#program to find average of two numbers entered by the user
+a=input("enter value of a")
+b=input("enter value of b")
+a=int(a)
+b=int(b)
+avg=(a+b)/2
+print("average of a and b is",avg)
+#program to print square of a number
+t=input("enter value of t")
+t=int(t)
+print(t*t)
+
+#to add space after taking input use comma
+name=input("enter your name")
+print("hello",name)
